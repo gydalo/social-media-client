13c941c1cc820d38796587db5ba0f3cc7283be62
gydalo <gylo_l@hotmail.com>
2024-10-15 11:24:53 +0200

    Added testing for the login.js

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,6 +2,13 @@ import globals from 'globals';
 import pluginJs from '@eslint/js';
 
 export default [
-  { languageOptions: { globals: globals.jest /*globals.browser*/ } },
+  {
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.jest,
+      },
+    },
+  },
   pluginJs.configs.recommended,
 ];
