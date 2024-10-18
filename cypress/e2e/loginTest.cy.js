ac6451f0b857f57f90dfce68f83932e9c32294f1
gydalo <gylo_l@hotmail.com>
2024-10-18 10:30:38 +0200

    Tried fixing Eslint cypress bug

--- cypress/e2e/loginTest.cy.js
+++ cypress/e2e/loginTest.cy.js
@@ -1,5 +1,5 @@
 /* eslint-disable */
-// Had to disable because I couldn't find a way to make EsLint understand Cypress. Will look more into it later.
+// Had to disable because I couldn't find a way to make EsLint understand Cypress.
 
 describe('Login Test', function () {
   beforeEach(() => {
